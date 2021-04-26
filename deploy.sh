17f3d13a07286731e24de95bc534ccd0599cbc58
陶翔 <thawsoar@gmail.com>
2021-04-26 17:17:21 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,13 @@ cd ./dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
+git config --global user.email "taoxiang"
+git config --global user.name "thawsoar@gmail.com"
 
 git init
-git config user.name "taoxiang"
-git config user.email "thawsoar@gmail.com"
 git add *
 
 git commit -m 'Travis CI Auto Builder Deploy'
-echo ${access_token}
 git push --force --quiet https://${access_token}@github.com/Thawsoar/vue-cms.git master:deploy
 
 ssh root@47.105.40.207 'cd ~/../data/www/middle && git fetch --all && git reset --hard origin/deploy && git pull'
