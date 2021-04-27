692b876bda114781ccfcd12656b4579102a04e7b
thawsoar@gmail.com <taoxiang>
2021-04-27 10:50:56 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git add *
 
 git commit -m 'Travis CI Auto Builder Deploy'
 # git push --force --quiet git@github.com:Thawsoar/vue-cms.git master:deploy
-git push -f https://${access_token}@github.com/Thawsoar/vue-cms.git master:deploy
+git push -f -q https://${GH_TOKEN}@g${GH_REF} master:deploy
 
 echo 'test'
 ssh root@47.105.40.207 'cd ~/../data/www/middle && git fetch --all && git reset --hard origin/deploy && git pull'
