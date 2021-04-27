d767093eaf8eabd9b344d097655e418962c2ccdf
thawsoar@gmail.com <taoxiang>
2021-04-27 14:26:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env sh
-###
- # @Author: taoxiang
- # @Date: 2021-04-27 14:14:59
- # @LastEditTime: 2021-04-27 14:14:59
- # @LastEditors: taoxiang
- # @Description: 
- # @FilePath: /blog/admin/deploy.sh
- # 打工人,打工魂,打工都是人上人
-### 
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -28,7 +19,7 @@ git add *
 
 git commit -m 'Travis CI Auto Builder Deploy'
 # git push --force --quiet git@github.com:Thawsoar/vue-cms.git master:deploy
-git push -f -q https://${GH_TOKEN}@g${GH_REF} master:deploy
+git push -f -q https://${GH_TOKEN}@${GH_REF} master:deploy
 
 ssh -o "StrictHostKeyChecking no" root@47.105.40.207 'cd /data/www/middle && rm -rf ./* && git clone -b deploy https://github.com/Thawsoar/vue-cms.git && mv vue-cms dist'
 
